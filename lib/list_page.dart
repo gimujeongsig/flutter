dc5741b8f7b224d186020cf31ca5465564f6f1d2
SMART24\User <dsa>
2025-04-07 13:27:31 +0900

    list 예제 글 쓰면 추가하는거 만들었다

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
-
 class ListPage extends StatefulWidget {
   const ListPage({super.key});
 
@@ -11,30 +8,57 @@ class ListPage extends StatefulWidget {
 }
 
 class _State extends State<ListPage> {
-  
   final List<String> fruits = ["사과", "바나나", "포도", "딸기", "복숭아"];
-  
-  
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("List 예제"),
-      ),
-      body: ListView.builder(
-        itemCount: fruits.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: const Icon(Icons.favorite),
-            title: Text(fruits[index]),
-            onTap: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text('${fruits[index]}를 선택했어요!'))
-              );
-            },
-          );
-        },
-      )
-    );
+        appBar: AppBar(
+          title: const Text("List 예제"),
+        ),
+        body: Column(
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                          controller: _controller,
+                          decoration: const InputDecoration(
+                              hintText: "아이템 입력", border: OutlineInputBorder()),
+                        )),
+                    const SizedBox(width: 10),
+                    ElevatedButton(
+                        onPressed: () {
+                          final text = _controller.text.trim();
+                          if (text.isNotEmpty){
+                            setState(() {
+                              fruits.add(text);
+                              _controller.clear();
+                            });
+                          }
+                        },
+                        child: const Text("추가")
+                    )
+                  ],
+                )),
+            const Divider(),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: fruits.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: const Icon(Icons.favorite),
+                        title: Text(fruits[index]),
+                        onTap: () {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text('${fruits[index]}를 선택하세요')));
+                        },
+                      );
+                    }))
+          ],
+        ));
   }
 }
