313e8e8152959225569cec4b0b4868dd82bb038e
gimujeongsig <dbehcuwjc@gmail.com>
2025-04-14 13:03:16 +0900

    initial commit

---
+++ lib/flex_page.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+
+import 'drawer.dart';
+
+class FlexPage extends StatefulWidget {
+  const FlexPage({super.key});
+
+  @override
+  State<FlexPage> createState() => _FlexPageState();
+}
+
+class _FlexPageState extends State<FlexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flexible 예제"),
+      ),
+      drawer: const MyDrawer(),
+      body: Row(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              color: Colors.lightBlue,
+              height: 200,
+            )
+          ),
+          Flexible(
+              flex: 2,
+              child: Container(
+                color: Colors.yellow,
+              )
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:hello/button_page.dart';
+import 'package:hello/flex_page.dart';
+import 'package:hello/image_page.dart';
+import 'package:hello/layout_page.dart';
+import 'package:hello/popup_page.dart';
+import 'package:hello/setting_page.dart';
+import 'package:hello/list_page.dart';
+import 'package:hello/ToDoPage.dart';
+
+import 'drawer.dart';
+import 'input_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: "/",
+      routes: {
+        '/': (context) => const LayoutPage(),
+        '/setting': (context) => const SettingPage(),
+        '/popup': (context) => const PopupPage(),
+        '/flex': (context) => const FlexPage(),
+        '/image': (context) => const ImagePage(),
+        '/button': (context) => const ButtonPage(),
+        '/input': (context) => const InputPage(),
+        "/list" : (context) => const ListPage(),
+        "/to-do" : (context) => const TodoPage(),
+      },
+    );
+  }
+}
+
+
+
+
+
+
+
